e721a8e4282dc66417277cd9ad230671979d2dec
geritol <gergo.tolnai@gmail.com>
2018-05-10 13:02:50 +0200

    feat(utils): added param to runCommand to resolve with the cmd output

--- src/utils/SystemUtils.js
+++ src/utils/SystemUtils.js
@@ -3,7 +3,7 @@
 import { exec } from 'child_process'
 import Logger from '../Logger'
 
-async function runCommand(command, showOutput = true) {
+async function runCommand(command, showOutput = true, resolveOutput = true) {
   Logger.debug(`Running command: ${command}`)
 
   const childProcess = exec(command, { cwd: process.cwd() }, error => {
@@ -11,12 +11,18 @@ async function runCommand(command, showOutput = true) {
       Logger.error(`exec error: ${error}`)
     }
   })
+  let data = ''
+  childProcess.stdout.on('data', chunck => data += chunck)
   if (showOutput) {
     childProcess.stdout.pipe(process.stdout)
   }
   return new Promise(resolve => {
-    childProcess.on('exit', () => {
-      resolve(true)
+    childProcess.on('exit', async () => {
+      if (resolveOutput) {
+        resolve(data)
+      } else {
+        resolve(true)
+      }
     })
   })
 }
