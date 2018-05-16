1d07ac44eb20bc5faa0c4d40559ba4928d0c87e0
geritol <gergo.tolnai@gmail.com>
2018-05-16 17:18:24 +0200

    fix(hook-tests): now reading hooks from tmp dir

--- src/GitHandler/Hooks.test.helper.js
+++ src/GitHandler/Hooks.test.helper.js
@@ -1,7 +1,3 @@
-import path from 'path'
-
-export const HOOK_FOLDER_PATH = path.join(__dirname, '../../scripts/hooks/')
-export const TEST_HOOK_PATH = `${HOOK_FOLDER_PATH}/pre-commit-test.js`
 export const TEST_HOOK_CONTENT = {
   ruleName: 'angular----test',
   execute: 'validate-commit-msg',
