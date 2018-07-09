f59b00e85f4760069cbeeb2c23bf313ad5f15645
Csaba Makara <csaba.makara@gmail.com>
2018-07-09 14:30:44 +0200

    chore(danger): added minimal config for danger (#31)
    * chore(danger): added minimal config for danger
    
    * chore(danger): test attempt
    
    * chore(danger): test attempt 2
    
    * chore(danger): fix import
    
    * chore(danger): fix import
    
    * chore(danger): fix import
    
    * chore(danger): fix import
    
    * chore(danger): added yarn check
    
    * chore(danger): added danger-plugin-yarn
    
    * chore(danger): use danger-plugin-yarn
    
    * chore(danger): improved dangerfile
    
    * chore(danger): fix import

---
+++ dangerfile.js
@@ -0,0 +1,38 @@
+const { message, danger, warn, fail, schedule } = require("danger")
+const { checkForNewDependencies, checkForLockfileDiff, checkForTypesInDeps } = require('danger-plugin-yarn')
+const includes = require("lodash/includes")
+
+if (danger.github.pr.body.length < 10) {
+  warn("Please include a description of your PR changes.");
+}
+
+if (!danger.github.pr.assignee) {
+  const method = danger.github.pr.title.includes("WIP") ? warn : fail
+  method("This pull request needs an assignee, and optionally include any reviewers.")
+}
+
+const packageChanged = includes(danger.git.modified_files, 'package.json');
+const lockfileChanged = includes(danger.git.modified_files, 'yarn.lock');
+if (packageChanged && !lockfileChanged) {
+  const message = 'Changes were made to package.json, but not to yarn.lock';
+  const idea = 'Perhaps you need to run `yarn install`?';
+  fail(`${message} - <i>${idea}</i>`);
+}
+
+var bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn('This pr seems to be a bit too big.')
+}
+
+if (danger.github.pr.deletions > danger.github.pr.additions) {
+  message(
+    `👏 Great job! I see more lines deleted than added. Thanks for keeping us lean!`
+  )
+}
+
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json")
+  checkForNewDependencies(packageDiff)
+  checkForLockfileDiff(packageDiff)
+  checkForTypesInDeps(packageDiff)
+})
---
+++ src/ProjectOptionHandler/ProjectOptionDescriptors/standardjs.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ruleName: 'standardjs src',
+  execute: 'standard',
+  dependencies: ['standard'],
+  scriptName: 'code:lint',
+  category: 'Code Tools',
+  checked: true
+}
